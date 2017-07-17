48d9a021f19ba7c584ecc44db45e18051514de75
Yuan Jiace <yjc_buaa@163.com>
2017-07-17 11:22:18 +0800

    delete type field in station model

--- models/station.py
+++ models/station.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python   
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 from sqlalchemy import Column, ForeignKey
@@ -13,7 +13,6 @@ station
 id int 10 primary key
 app_id int 10
 name varchar 50
-type varchar 50
 location varchar 100
 lat decimal 10
 lon decimal 11
@@ -29,7 +28,6 @@ class Station(Base):
 	__tablename__ = 'station'
 	id = Column(INTEGER(display_width = 10), primary_key = True)
 	name = Column(VARCHAR(length = 50))
-	type = Column(VARCHAR(length = 50))
 	location = Column(VARCHAR(length = 100))
 	lat = Column(DECIMAL)
 	lon = Column(DECIMAL)
